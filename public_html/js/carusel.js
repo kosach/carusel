5f00c7781d74dade4f7d852f12dbd6b225600ae5
a.kosach <kosach.a@gmail.com>
2015-10-09 16:09:16 +0300

    Получение информации ил=з блоков и расспрложениее ее по местам определенным для элементов

--- public_html/js/carusel.js
+++ public_html/js/carusel.js
@@ -1,2 +1,38 @@
-console.log('test script');
-
+BlocsForPrint={
+    Blocs : function(){        
+        return document.querySelectorAll(".carusel-inner");
+    },
+    hideBlocs:function(){
+        var blocs = this.Blocs();
+        console.log(blocs);
+        for(var i=0;i<blocs.length;i++){
+            blocs[i].style.cssText = "display:none;";
+        }       
+        return 3;
+    }
+};
+ElemOnPage ={
+    Elems : function(){
+       return document.querySelectorAll(".carusel-place")
+    },
+    maxElem:function(){
+        var elements = document.querySelectorAll(".carusel-place");
+        return elements.length;
+    },
+    putBlocsOnPage:function(){
+        var blocs = BlocsForPrint.Blocs();
+        var elems = this.Elems();
+        var i;
+        var maxElems;
+        
+        if(elems.length<blocs.length){
+            maxElems = elems.length;           
+        }else{
+            maxElems =blocs.length;
+        }
+        for(i=0;i<maxElems;i++){
+            elems[i].innerHTML = blocs[i].innerHTML;
+        }
+        return 3;
+    }   
+};
