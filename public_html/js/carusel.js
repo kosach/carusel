83f959f964db9d81d8d5b3306fa81656f0850e21
a.kosach <kosach.a@gmail.com>
2015-10-11 14:47:40 +0300

    Законченое получение параметров и вывод на экран

--- public_html/js/carusel.js
+++ public_html/js/carusel.js
@@ -1,38 +1,43 @@
-BlocsForPrint={
-    Blocs : function(){        
-        return document.querySelectorAll(".carusel-inner");
-    },
-    hideBlocs:function(){
-        var blocs = this.Blocs();
-        console.log(blocs);
-        for(var i=0;i<blocs.length;i++){
-            blocs[i].style.cssText = "display:none;";
-        }       
-        return 3;
-    }
-};
-ElemOnPage ={
-    Elems : function(){
-       return document.querySelectorAll(".carusel-place")
-    },
-    maxElem:function(){
-        var elements = document.querySelectorAll(".carusel-place");
-        return elements.length;
-    },
-    putBlocsOnPage:function(){
-        var blocs = BlocsForPrint.Blocs();
-        var elems = this.Elems();
-        var i;
-        var maxElems;
-        
-        if(elems.length<blocs.length){
-            maxElems = elems.length;           
-        }else{
-            maxElems =blocs.length;
+(function() {
+
+    BlocsForPrint={
+        Blocs : function(){        
+            return document.querySelectorAll(".carusel-inner");
+        },
+        hideBlocs:function(){
+            var blocs = this.Blocs();
+            //console.log(blocs);
+            for(var i=0;i<blocs.length;i++){
+                blocs[i].style.cssText = "display:none;";
+            }       
+            return 3;
         }
-        for(i=0;i<maxElems;i++){
-            elems[i].innerHTML = blocs[i].innerHTML;
-        }
-        return 3;
-    }   
-};
+    };
+    ElemOnPage ={
+        Elems : function(){
+           return document.querySelectorAll(".carusel-place")
+        },
+        maxElem:function(){
+            var elements = document.querySelectorAll(".carusel-place");
+            return elements.length;
+        },
+        putBlocsOnPage:function(){
+            var blocs = BlocsForPrint.Blocs();
+            var elems = this.Elems();
+            var i;
+            var maxElems;
+
+            if(elems.length<blocs.length){
+                maxElems = elems.length;           
+            }else{
+                maxElems =blocs.length;
+            }
+            for(i=0;i<maxElems;i++){
+                elems[i].innerHTML = blocs[i].innerHTML;
+            }
+            return 3;
+        }   
+    };
+    BlocsForPrint.hideBlocs();
+    ElemOnPage.putBlocsOnPage();
+})();
